ba7d0bbac668537fd7ddbc988ff090557fb9b779
robalb <alberto.ventafridda@gmail.com>
2018-02-14 22:37:49 +0100

    aggiunta struttura principale del sito

---
+++ resources/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+	
+	
+});
+
+function opencard(){
+	alert("quando cliccata, si aprirà a tutto schermo una versione ingrandita della carta, con info sui voti ricevuti, descrizione completa, e tasto vota.")
+}
