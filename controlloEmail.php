8ceb310344a00820b92ac79b2004e38ad7daa18c
slendy666 <slendy666@users.noreply.github.com>
2018-02-14 22:49:53 +0100

    Revert "Revert "create a connection to local db""
    This reverts commit 74955cb97b6945f4763ccdd9ff6a66f62d7deefa.

---
+++ controlloEmail.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+$votoImg1 = $_POST[];
+$votoImg2 = $_POST[];
+$votoImg3 = $_POST[];
+$votoImg4 = $_POST[];
+$votoImg5 = $_POST[];
+include "function/connessionedb.php";
+
+?>
+</body>
+</html>
